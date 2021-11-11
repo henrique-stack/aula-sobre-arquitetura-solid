8220ac0c5b28f67b87a34ce85e8f82714c4b7f0b
Henrique Lima <henriquelimaribeiro4@gmail.com>
2021-11-11 01:31:27 -0300

    without array

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,16 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.create({
+      email,
+      name,
+    });
+
+    if (user.email === email) {
+      throw new Error("email already exists");
+    }
+
+    return user;
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,10 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const admin = this.usersRepository.findById(user_id);
+    this.usersRepository.turnAdmin(admin);
+
+    return admin;
   }
 }
 
