606efa9e1c500ac9afaea8f3ece962a44397c411
Henrique Lima <henriquelimaribeiro4@gmail.com>
2021-11-16 15:48:10 -0300

    My commit test

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,7 +8,7 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-    try {
+    try {       
       const admin = this.listAllUsersUseCase.execute(typeof user_id);
 
       return response.status(201).send(admin);
